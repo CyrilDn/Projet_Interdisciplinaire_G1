7e341c8263aca309796683088d8fb8643bd95e73
CyrilDn <164194604+CyrilDn@users.noreply.github.com>
2025-12-16 10:14:59 +0100

    feat(brassin): Création de la classe brassin et ajout des attributs (#7)

---
+++ models/brassin.php
@@ -0,0 +1,17 @@
+<?php
+
+// connexion à la DB
+
+class brassin{
+
+    private $nom_table = "brassin";
+    private $connexion;
+
+    public $brassin_id;
+    public $nom;
+    public $dates;
+    public $volumes;
+    public $responsable;
+    public $statut;
+
+}
