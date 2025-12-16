a2d7eb65d926d55a71e5ce8b63c536f3e2367f7d
CyrilDn <cyril.denis@std.heh.be>
2025-12-16 15:08:07 +0100

    fix(fichiers): Modification des fichiers brassin et brassinModele

--- fichiers/brassin.php
+++ fichiers/brassin.php
@@ -4,26 +4,26 @@
 
 class brassin{
     private $brassin_id;
-    private $nom;
-    private $dates;
-    private $volumes;
+    private $nom_brassin;
+    private $date_debut;
+    private $volume;
     private $responsable;
     private $statut;
 
-    public function __construct($brassin_id, $nom, $dates, $volumes, $responsable, $statut){
+    public function __construct($brassin_id, $nom_brassin, $date_debut, $volume, $responsable, $statut){
 
         $this->brassin_id = $brassin_id;
-        $this->nom = $nom;
-        $this->dates = $dates;
-        $this->volumes = $volumes;
+        $this->nom = $nom_brassin;
+        $this->dates = $date_debut;
+        $this->volumes = $volume;
         $this->responsable = $responsable;
         $this->statut = $statut;
     }
     public function getBrassinId(){
         return $this->brassin_id;
     }
-    public function getNom(){
-        return $this->nom;
+    public function getNomBrassin(){
+        return $this->nom_brassin;
     }
 
     //... finir les autres getters et setters ici ...
