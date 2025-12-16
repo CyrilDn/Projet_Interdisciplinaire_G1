02bee531ddb13cac181519aa85742f65da437eda
CyrilDn <164194604+CyrilDn@users.noreply.github.com>
2025-12-16 09:53:14 +0100

    feat(projet): Ajout d'une base de données test (#5)

---
+++ db/brassin.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 16, 2025 at 08:50 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brassin`
+--
+
+CREATE TABLE `brassin` (
+  `id_brassin` int NOT NULL,
+  `nom` varchar(1000) NOT NULL,
+  `dates` date NOT NULL,
+  `volumes` int NOT NULL,
+  `responsable` varchar(1000) NOT NULL,
+  `statut` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `brassin`
+--
+
+INSERT INTO `brassin` (`id_brassin`, `nom`, `dates`, `volumes`, `responsable`, `statut`) VALUES
+(1, 'Brassin Blonde', '2025-12-16', 40, 'Pierre', 1),
+(2, 'Brassin Brune', '2025-12-16', 35, 'Daniel', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `brassin`
+--
+ALTER TABLE `brassin`
+  ADD PRIMARY KEY (`id_brassin`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `brassin`
+--
+ALTER TABLE `brassin`
+  MODIFY `id_brassin` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
