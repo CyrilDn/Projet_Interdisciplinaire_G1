7b3d3586e42e8cf39b0e1012e75d7681392b9120
CyrilDn <164194604+CyrilDn@users.noreply.github.com>
2025-12-17 09:33:32 +0100

    feat(evenementModele): ajout du contenu pour evenementModele (#22)

---
+++ models/evenementModele.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once 'fichiers/evenement.php';
+require_once 'connexionbd.php';
+
+class evenementModele{
+  
+    private $conn;
+    public function __construct(){
+        $db = New connexionbd();
+        $this->conn = $db->getbd();
+    }
+
+    public function liredb() {
+        $bdd = $this->conn; 
+        $resultat = $bdd->query("SELECT id_evenement, type_evenement, debut_evenement, fin_evenement, responsable, id_employe FROM evenement");
+        $evenement = [];
+        while ($ligne = $resultat->fetch()) { //pour chaque ligne on crée un objet evenement en utilisant notre constructeur
+            $evenement[] = new Brassin(
+                $ligne['id_evenement'],
+                $ligne['type_evenement'],
+                $ligne['debut_evenement'],
+                $ligne['fin_evenement'],
+                $ligne['responsable'],
+                $ligne['id_employe']
+            );
+        }
+        return $evenement;
+    }
+}
