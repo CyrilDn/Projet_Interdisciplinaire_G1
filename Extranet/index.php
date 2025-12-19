fa7287359c6ec2579c21793d3994ccb4711fe7d5
CyrilDn <164194604+CyrilDn@users.noreply.github.com>
2025-12-19 11:58:35 +0100

    feat(extranet): Ajout du css pour l'extranet (#58)

--- Extranet/index.php
+++ Extranet/index.php
@@ -24,16 +24,24 @@ if (isset($_GET ['tables'])) {
 <head>
     <meta charset="UTF-8">
     <title>Brassin d'Or</title>
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
+    <div class="conteneur">
+    <div class="bloc-secondaire">
     <h2>Extranet :</h2>
     <form method="get" action="index.php">
+        <div class="petit-element">
         <label>Choisir une table à afficher :</label>
+ 
         <select name="tables">
             <option value="evenement">Événements</option>
             <option value="produit">Produits</option>
         </select>
         <input type="submit" value="Consulter">
+        </div>
+    </div>
+    </div>
     </form>
     <br>
 </body>
--- Extranet/vues/evenementVue.php
+++ Extranet/vues/evenementVue.php
@@ -2,11 +2,11 @@
     require_once 'fichiers/evenement.php';
 ?>
 
-<div>
+<div class="bloc-centre">
 <?php
     foreach($bd as $ligne) {
 
-        echo '<div>';
+        echo '<div class="element">';
         echo '<h3>Evenement : ' . $ligne->getTypeEvent() . '</h3>';
         echo '<p>L\'événement se déroulera du ' . $ligne->getDebutEvent() .' au '. $ligne->getFinEvent().'</p>';
         echo '<p>Employé responsable de l\'événement : ' . "//" . '</p>';
