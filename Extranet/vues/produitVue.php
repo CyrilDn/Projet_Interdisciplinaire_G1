c8b86ecbcb28969e1b14a32ec445577b593ae4d0
CyrilDn <cyril.denis@std.heh.be>
2025-12-19 11:57:59 +0100

    feat(extranet): Ajout du css pour l'extranet

--- Extranet/vues/produitVue.php
+++ Extranet/vues/produitVue.php
@@ -2,11 +2,11 @@
     require_once 'fichiers/produit.php';
 ?>
 
-<div>
+<div class="bloc-centre">
 <?php
     foreach($bd as $ligne) {
         
-        echo '<div>';
+        echo '<div class="element">';
         echo '<h3>Produit : ' . $ligne->getNom() . '</h3>';
         echo '<p>Quantité disponible : '. $ligne->getQuantite() .' Unités</p>';
         echo '<p>Prix : '. $ligne->getPrix() .'€/L  </p>';
